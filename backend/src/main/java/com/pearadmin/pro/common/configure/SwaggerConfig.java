aa4dbd9545a35792fecd3267ba756d5272eab933
gaoqs <992711504@qq.com>
2022-03-04 21:20:32 +0800

    解决接口文档菜单跳转异常

--- backend/src/main/java/com/pearadmin/pro/common/configure/SwaggerConfig.java
+++ backend/src/main/java/com/pearadmin/pro/common/configure/SwaggerConfig.java
@@ -7,7 +7,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import java.util.ArrayList;
 
 /**
@@ -17,7 +17,7 @@ import java.util.ArrayList;
  * CreateTime: 2021/04/01
  * */
 @Configuration
-@EnableSwagger2
+@EnableOpenApi
 public class SwaggerConfig  {
 
     private static final String BASE_PACKAGE = "com.pearadmin";
